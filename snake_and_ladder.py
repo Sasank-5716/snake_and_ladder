6cab36184f7532607fa98615462b43f86e583069
Sasank5716 <sasanklama5716@gmail.com>
2025-04-14 10:59:04 +0545

    Add colors and game constants

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -8,3 +8,17 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snakes & Ladders")
 clock = pygame.time.Clock()
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 128, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Game Constants
+BOARD_SIZE = 10
+CELL_SIZE = 50
+PLAYER_SIZE = 20
+DICE_SIZE = 50
